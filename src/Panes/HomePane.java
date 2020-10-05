bafbdf190d3ed1de41a719942305ff32bc56e38d
Renyi Yuan <ryyuan98@gmail.com>
2020-10-05 19:22:59 +0800

    "utf-8 encoding"

--- src/Panes/HomePane.java
+++ src/Panes/HomePane.java
@@ -11,23 +11,23 @@ import javafx.scene.text.Font;
 
 public class HomePane extends BorderPane {
 
-	Label homeTitle;		//用于显示界面上的标题
-	ImageView gameLogo;		//用于显示游戏logo
-	Button newgameBtn;		//开始新游戏按钮
+	Label homeTitle;	
+	ImageView gameLogo;		
+	Button newgameBtn;	
 	
 	public HomePane() {
 		
-		//顶端放标题	
+		
 		homeTitle = new Label("Tomb Escape");
 		homeTitle.setFont(Font.font("Arial",30));
 		this.setTop(homeTitle);
 		
-		//中间放logo
-		Image iconImage = new Image("imgs/tomb.jpg");
+		
+		Image iconImage = new Image("file:images/tomb.jpg");
 		gameLogo.setImage(iconImage);
 		this.setCenter(gameLogo);
 		
-		//底部放开始按钮
+		
 		newgameBtn = new Button("New Game");
 		this.setBottom(newgameBtn);
 	}
--- src/Panes/SettingPane.java
+++ src/Panes/SettingPane.java
@@ -10,43 +10,43 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class SettingPane extends VBox{
-	Label settingTitle;			//主标题
-	ImageView[] characterIcon;	//每一个角色的icon
-	Label propsHP,propsPRO;				//角色属性
-	Button startBtn;			//游戏开始按钮
+	Label settingTitle;			
+	ImageView[] characterIcon;	
+	Label propsHP,propsPRO;				
+	Button startBtn;			
 	String[][] characterProps = new String[][] {
 		{"3 points","1 spade"},
 		{"3 points","2 recovery"},
 		{"3 points","1 invisible"}
-	};							//每一个角色的属性表
+	};						
 	
 	ToggleGroup modeGroup;
 	RadioButton[] modeBtn;
 
 	public SettingPane() {
 		
-		//放在头部的标题
+		
 		settingTitle = new Label("Settings");
 		this.getChildren().add(settingTitle);
 		
-		//读取图片资源放在一个HBox里,然后把HBox放在title下
+		
 		HBox iconBox = new HBox();
 		iconBox.setSpacing(20);
 		Image[] imageRead = new Image[3];		
 		for (int i=0; i<imageRead.length; i++) {
-			imageRead[i] = new Image("character"+String.valueOf(i));
+			imageRead[i] = new Image("file:images/character"+String.valueOf(i));
 			characterIcon[i].setImage(imageRead[i]);
 			iconBox.getChildren().add(characterIcon[i]);
 		}
 		this.getChildren().add(iconBox);
 		
-		//把HP和PRO两个标签加入,悬停时更新
+		
 		propsHP = new Label("HP:");
 		propsPRO = new Label("PRO");
 		this.getChildren().add(propsHP);
 		this.getChildren().add(propsPRO);
 
-		//加入模式选择单选按钮,togglegroup 没办法直接加所以只能定义一个HBOX在这里
+		
 		HBox modeBtnBox = new HBox();
 		modeGroup = new ToggleGroup();
 		modeBtn = new RadioButton[3];
@@ -59,7 +59,7 @@ public class SettingPane extends VBox{
 		}
 		this.getChildren().add(modeBtnBox);
 		
-		//加入游戏开始按钮
+		
 		startBtn = new Button("Start");
 		this.getChildren().add(startBtn);
 		
