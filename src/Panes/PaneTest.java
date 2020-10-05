d0b455fd0394ba8e09cbcd8ca5c7c2f7feb506d4
Renyi Yuan <ryyuan98@gmail.com>
2020-10-05 19:28:38 +0800

    add panetest

---
+++ src/Panes/PaneTest.java
@@ -0,0 +1,25 @@
+package Panes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PaneTest extends Application {
+
+	@Override
+	public void start(Stage root) {
+		// TODO Auto-generated method stub
+		HomePane homePane = new HomePane();
+		SettingPane settingPane = new SettingPane();
+		Scene homeScene = new Scene(homePane);
+		Scene settingScene = new Scene(settingPane);
+		root.setScene(homeScene);
+		root.setTitle("Tomb-Escape");
+		root.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
